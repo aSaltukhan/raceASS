62339a23917d45b80ef4b2a4b6dfe526c755d8af
aSaltukhan <64299562+aSaltukhan@users.noreply.github.com>
2026-01-21 16:11:57 +0300

    Refactor imports and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/raceASS/",
   plugins: [react()],
-  base: '/raceASS/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 });
